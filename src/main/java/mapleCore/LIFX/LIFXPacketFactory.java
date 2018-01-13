8531cc262513e0b65d37a4f0f0dddbabdac41ea5
williamallen <wallen2268@gmail.com>
2018-01-12 21:21:32 -0600

    First commit of Maple

---
+++ src/main/java/mapleCore/LIFX/LIFXPacketFactory.java
@@ -0,0 +1,29 @@
+package mapleCore.LIFX;
+
+/**
+ * Created by williamallen on 1/9/18.
+ */
+public class LIFXPacketFactory {
+
+    
+
+    public LIFXPacketFactory(){
+
+    }
+
+    /**
+     * There are a multitude of parts that need to be
+     *
+     *
+     *
+     */
+
+    public static byte[] createInstruction(){
+
+    }
+
+
+
+
+}
+
---
+++ src/main/java/mapleCore/init/Maple.java
@@ -0,0 +1,13 @@
+package mapleCore.init;
+
+/**
+ * Created by williamallen on 12/27/17.
+ */
+public class Maple {
+
+    public static void Main(){
+
+    }
+
+
+}
